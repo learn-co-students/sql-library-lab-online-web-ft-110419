113617bf15f4f0cd8e1fb7f941d67a2bd23def73
xdundun <xdundun@gmail.com>
2020-01-05 11:28:42 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,49 @@
+INSERT INTO series VALUES (1, "The Witcher", 1,1);
+INSERT INTO series VALUES  (2, "Halo",2,2);
+INSERT INTO subgenres VALUES (1, "Fantasy");
+INSERT INTO subgenres VALUES (2, "Sci-Fi");
+INSERT INTO authors VALUES (1,"Andrzej Sapkowski");
+INSERT INTO authors VALUES (2, "Eric Nylund");
+INSERT INTO books VALUES (1,"The Last Wish",1993,1);
+INSERT INTO books VALUES (2,"Sword of Destiny",1992,1);
+INSERT INTO books VALUES (3,"Blood of Elves",1994,1);
+INSERT INTO books VALUES (4,"The Fall of Reach",2001,2);
+INSERT INTO books VALUES (5, "Ghosts of Onyx", 2006,2);
+INSERT INTO books VALUES (6, "First Strike",2003,2);
+INSERT INTO characters VALUES (1, "Geralt", "human", "2 swords, both for killing monsters", 1);
+INSERT INTO characters VALUES (2, "Yennefer", "human", "smells like lilac and gooseberries",1);
+INSERT INTO characters VALUES (3, "Cirilla", "human", "hatred and vengeance blinded me",1);
+INSERT INTO characters VALUES (4, "Vesemir", "human", "she will be the end of me",1);
+INSERT INTO characters VALUES (5, "Eredin", "elf", "The wild hunt rides",1);
+INSERT INTO characters VALUES (6, "John", "human", "Permission sir? to give them back the bomb",2);
+INSERT INTO characters VALUES (7, "Cortana", "ai", "Remeber, I choose you",2);
+INSERT INTO characters VALUES (8, "Kelly", "human", "Noone has a better shot",2);
+INSERT INTO character_books VALUES (1,1,1);
+INSERT INTO character_books VALUES (2,1,2);
+INSERT INTO character_books VALUES (3,3,4);
+INSERT INTO character_books VALUES (4,2,1);
+INSERT INTO character_books VALUES (5,3,1);
+INSERT INTO character_books VALUES (6,3,2);
+INSERT INTO character_books VALUES (7,3,3);
+INSERT INTO character_books VALUES (8,4,6);
+INSERT INTO character_books VALUES (9,5,6);
+INSERT INTO character_books VALUES (10,6,6);
+INSERT INTO character_books VALUES (11,3,4);
+INSERT INTO character_books VALUES (12,2,4);
+INSERT INTO character_books VALUES (13,2,5);
+INSERT INTO character_books VALUES (14,3,5);
+INSERT INTO character_books VALUES (15,6,7);
+INSERT INTO character_books VALUES (16,6,8);
+
+
+
+
+
+
+
+
+
+
+
+
+
